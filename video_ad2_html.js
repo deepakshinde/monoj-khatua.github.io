7d6767afcb2d2f290b7ffd18c74b2c4169aa8b2b
monoj-khatua <monoj.khatua@coriolis.co.in>
2016-03-14 20:33:01 +0530

    added html player related pages

---
+++ video_ad2_html.js
@@ -0,0 +1,148 @@
+/*
+ * 2nd stage. This is where we put in code talking to jwplayer/videojs to
+ * implement commands coming from the parent
+ */
+
+var state = "";
+var rb_vastTag = [];
+
+function parent_cmd(cmd) {
+    console.log("From parent: ", cmd);
+    if (state === "end") {
+        parent_cb("end");
+    } else if (cmd === "play") {
+        state = "play";
+        playerInstance.play(true);
+        //simulateEnd();
+        parent_cb(cmd);
+    } else if (cmd === "pause") {
+        state = "pause";
+        playerInstance.play(false);
+        parent_cb(cmd);
+    } else if (cmd === "start") {
+        playerInstance.play(true);
+    } else {
+        console.log("Unknown command from parent");
+    }
+}
+
+function simulateEnd() {
+    setTimeout(function() {
+        $("video").slideUp(1000, function() {
+            state = "end";
+            parent_cb("end");
+        });
+    }, 30000);
+}
+
+//var fallbacks =["http://demo.tremorvideo.com/proddev/vast/vast1RegularLinear.xml"]
+//Liverail
+//var fallbacks =["http://dspbuilder.rubiconproject.com/vast?cid=56d039ea70726f2631000005&userid=123213&t=imp&oid=55a5e2a370726f07db010000&aid=56d439be70726f0e77000001&bid_id=43434&impid=213123&auctionid=1231231&exchange=rubicon&price=0.023&dc=us-west&ct=0"]
+//var fallbacks = ["http://dspbuilder.rubiconproject.com/track?cid=56d039ea70726f2631000005&userid=${USERID}&t=imp&oid=55a5e2a370726f07db010000&aid=55e6f99e70726f705000000b&&rurl=http%3A%2F%2Fs3.amazonaws.com%2Fopenplatform-apps%2F55e6f99e70726f705000000b-%2456d039ea70726f2631000005.xml","//www.adotube.com/php/services/player/OMLService.php?avpid=oRYYzvQ&platform_version=vast20&ad_type=linear&groupbypass=1&HTTP_REFERER=http://www.longtailvideo.com&video_identifier=longtailvideo.com,test"];
+
+rb_vastTag = window.parent._rbVastTag;
+console.log(rb_vastTag[0]);
+var playerInstance = jwplayer("video-container");
+playerInstance.setup({
+    file: "//content.jwplatform.com/videos/bkaovAYt-640.mp4",
+    //image: "/customer/portal/attachments/268131",
+    width: '100%',
+    height: '100%',
+    stretching: 'exactfit',
+    //controls: false,
+    //controlbar: 'none',
+    icons: false,
+    mute: true,
+    //primary: 'flash',
+    aspectratio: '16:9',
+    skin: {
+        name: "custom_skin"
+    },
+    logo: {
+        file: "rubiconproject_circles.png",
+        hide: false,
+        position: "top-right",
+        link: "http://rubiconproject.com"
+    },
+    advertising: {
+    client: "vast",
+    tag: rb_vastTag
+    }});
+
+playerInstance.setMute(true);
+
+
+function mute(){
+    playerInstance.setMute(true);
+}
+
+function unmute(){
+    playerInstance.setMute(false);
+}
+
+var index = 0;
+playerInstance.on("AdError" ,function(event) {
+    console.log("AdError");
+    console.log(event.message);
+    /*var html = log.innerHTML;
+    if(index < fallbacks.length) {
+        html += event.tag+" was empty, loading fallback tag "+(index+1)+".<br>";
+        console.log(html);
+        index++;
+    } else {
+        html += event.tag+" has failed, with no more fallbacks available.<br>";
+        console.log(html);
+        parent_cb("end");
+         }*/
+    parent_cb("end");
+    });
+
+playerInstance.on("complete",function(){
+    console.log("Video Compete");
+    parent_cb("end");
+});
+
+playerInstance.onAdImpression(function(event) {
+    var html ="Success! Ad tag number "+ (index+1) +" is now playing!</br>";
+    parent_cb("started");
+    playerInstance.play(false);
+    console.log(html);
+    });
+/*
+playerInstance.on("adBlock",function(){
+    console.log("AdBlocked");
+    parent_cb("error");
+});
+*/
+playerInstance.on("complete",function(){
+    console.log("Video Compete");
+    parent_cb("end");
+});
+
+playerInstance.on("setupError",function(){
+    console.log("Video setup error");
+    parent_cb("error");
+});
+
+playerInstance.on("ready",function(){
+    console.log("Video ready");
+    parent_cb("ready");
+});
+
+
+playerInstance.on("adComplete", function(event){
+        console.log("Ad Completed");
+        //playerInstance.remove();
+        parent_cb('end');
+    });
+
+$("body").mouseover(function(){
+    unmute();
+    console.log("unmute");
+});
+
+$("body").mouseout(function(){
+    mute();
+    console.log("mute");
+});
+
---
+++ video_ad_html.js
@@ -0,0 +1,227 @@
+/*
+ * Ad loader, stage 1
+ */
+
+(function() {
+
+var iframe_id = "rbvideo_" + ord,
+    width = 640,
+    height = 360,
+    fraction = 0.5,
+    tpl = '<div id="canary_' + iframe_id + '"></div><div style="margin: 0; padding: 0; overflow: hidden; display:none" id="div_' + iframe_id + '"><iframe id="' + iframe_id + '" frameborder="0" width="' + width + '" height="' + height + '"></iframe></div>',
+    state = "init",
+    $ = undefined;
+var player="init";
+document.write(tpl);
+script_add("//code.jquery.com/jquery-1.12.1.min.js", function(err) {
+    if (err) {
+        console.log("Ad loader: could not load jquery");
+        return;
+    }
+
+    $ = window["jQuery"].noConflict(true);
+    main();
+});
+
+function main() {
+
+    var frame = document.querySelectorAll("#" + iframe_id)[0];
+
+    /*
+     * Messages from iframe processed here
+     */
+
+    window[iframe_id] = function(ev) {
+        console.log("From child: " + ev);
+        if (ev === "loaded") {
+            state = "loaded";
+        } else if (ev === "end") {
+            $("#div_" + iframe_id).slideUp(1000, function() {
+                $("#div_" + iframe_id).remove();
+                state = "init";
+            });
+        } else if (ev === "ready") {
+            player = "ready";
+        } else if (ev === "error") {
+            $("#div_" + iframe_id).remove();
+            state = "init";
+        }else if(ev === "started"){
+            $("#div_" + iframe_id).slideDown(1000, function(){
+                state = "started";
+                checkScroll();
+            });
+        }else {
+            state = ev;
+        }
+    };
+
+    function send_iframe_cmd(cmd) {
+        if (state === "init" || state === "loaded") {
+            console.log("IFrame not yet ready: " + cmd);
+            return;
+        }
+        frame.contentWindow.parent_cmd(cmd);
+    }
+
+
+    /* main visibility API function 
+     check if current tab is active or not*/
+    var tab_visibility = (function(){
+        var stateKey, 
+            eventKey, 
+            keys = {
+                    hidden: "visibilitychange",
+                    webkitHidden: "webkitvisibilitychange",
+                    mozHidden: "mozvisibilitychange",
+                    msHidden: "msvisibilitychange"
+        };
+        for (stateKey in keys) {
+            if (stateKey in document) {
+                eventKey = keys[stateKey];
+                break;
+            }
+        }
+        return function(c) {
+            if (c) document.addEventListener(eventKey, c);
+            return !document[stateKey];
+        }
+    })();
+    
+    /* check if current tab is active or not*/
+    tab_visibility(function(){
+        if(tab_visibility()){  
+              setTimeout(function(){ 
+                send_iframe_cmd("play");
+            },300);
+        } else {
+            send_iframe_cmd("pause");
+            console.log("Out of tab");
+        }
+    });
+    
+    
+    /* check if browser window has focus*/        
+    var notIE = (document.documentMode === undefined),
+        isChromium = window.chrome;
+          
+    if (notIE && !isChromium) {
+    
+        // checks for Firefox and other  NON IE Chrome versions
+        $(window).on("focusin", function () { 
+            
+            setTimeout(function(){      
+                send_iframe_cmd("play");
+            },300);
+        }).on("focusout", function () {
+            send_iframe_cmd("pause");
+        });
+    
+    } else {
+        // checks for IE and Chromium versions
+        if (window.addEventListener) {
+            window.addEventListener("focus", function (event) {
+                setTimeout(function(){                 
+                     send_iframe_cmd("play");
+                },300);
+            }, false);
+    
+            window.addEventListener("blur", function (event) {
+                send_iframe_cmd("pause");
+            }, false);
+        } else {
+            window.attachEvent("focus", function (event) {
+                setTimeout(function(){                 
+                     send_iframe_cmd("play");
+                },300);
+            });
+    
+            window.attachEvent("blur", function (event) {
+                send_iframe_cmd("pause");
+            });
+        }
+    }
+
+    function checkScroll() {
+        var off = $("#canary_" + iframe_id).offset(),
+            x = off.left, y = off.top, w = width, h = height, r = x + w,
+            b = y + h, //bottom
+            visibleX, visibleY, visible;
+
+        visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+        visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+        visible = visibleX * visibleY / (w * h);
+
+        if (visible > fraction) {
+            if (state === "loaded" && player === "ready") {
+                /*$("#div_" + iframe_id).slideDown(1000, function() {
+                    state = "ready";
+                    send_iframe_cmd("play");
+                });*/
+                state = "ready";
+                send_iframe_cmd("start");
+
+            } else if (state !== "ready" && state !== "play") {
+                send_iframe_cmd("play");
+            }
+        } else if (state === "play") {
+            send_iframe_cmd("pause");
+        }
+    }
+
+    var tpl2 = '<!DOCTYPE html>' +
+        '<html>' +
+        '<head>' +
+        '<style> .simple { margin:0; padding:0, overflow:hidden; } </style>' +
+        '<scr' + 'ipt src="//content.jwplatform.com/libraries/XeGdlzmk.js"></scr'+'ipt>'+
+        '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,600italic,400italic" rel="stylesheet" type="text/css">'+
+        '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">' +
+        '<link href="//desk-customers.s3.amazonaws.com/jwplayer/css/main.css" rel="stylesheet">' +
+        '<link href="//monoj-khatua.github.io/custom_skin.css" rel="stylesheet">' +
+        '</head>' +
+        '<body class="simple">' +
+        '<scr' + 'ipt type="text/javascript">' +
+        'var parent_cb = window.parent["' + iframe_id + '"];' +
+        'document.addEventListener("DOMContentLoaded", function() { parent_cb("loaded"); });' +
+        '</scr' + 'ipt>' +
+        '<div id="video-container" style="position:absolute;top:0px;left:0px;z-index:1;background-color:black">' +
+        '</div>' +
+        '<scr' + 'ipt type="text/javascript" src="//code.jquery.com/jquery-1.12.1.min.js"></scr' + 'ipt>' +
+        '<scr' + 'ipt type="text/javascript" src="//monoj-khatua.github.io/video_ad2_html.js?ord='+ord +'"></scr' + 'ipt>' +
+        '</body>' +
+        '</html>';
+
+    frame.contentWindow.document.open("text/html", "replace");
+    frame.contentWindow.document.write(tpl2);
+    frame.contentWindow.document.close();
+
+    document.addEventListener("resize", checkScroll, false);
+    document.addEventListener('scroll', checkScroll, false);
+}
+
+/* Based on JQuery's loader */
+function script_add(url, cb) {
+    var script,
+        head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+
+    script = document.createElement("script");
+    script.src = url;
+    script.onload = script.onreadystatechange = script.onerror = function(e,
+        isAbort) {
+        isAbort = isAbort || e.type === 'error';
+        if (isAbort || !script.readyState ||
+            /loaded|complete/.test(script.readyState)) {
+
+            script.onload = script.onreadystatechange = null;
+            if (isAbort && head && script.parentNode) {
+                head.removeChild(script);
+            }
+            script = undefined;
+            var ret = isAbort ? "Error loading script: " + url : null;
+            return cb(ret, null);
+        }
+    };
+    head.insertBefore(script, head.firstChild);
+}
+
+})();
